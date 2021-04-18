395b2176eec8b120e688fdef48c1c42a7faae1b5
RamenOverdose <wangallen98@gmail.com>
2021-04-18 18:19:08 -0500

    dark theme

--- indexScript.js
+++ indexScript.js
@@ -9,4 +9,7 @@ function openLink(element) {
     else if (id === "photography") {
         window.open("https://www.flickr.com/photos/ramenoverdose/albums/72157696885465250", '_blank');
     }
+    else if (id === "instagram") {
+        window.open("https://www.instagram.com/superokay.img/", '_blank');
+    }
 }
