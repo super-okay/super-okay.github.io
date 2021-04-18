9b28bfd53f1dc3e0105a265731c94c4261365cbf
RamenOverdose <wangallen98@gmail.com>
2021-04-18 17:47:01 -0500

    revamping github personal site

---
+++ indexScript.js
@@ -0,0 +1,12 @@
+function openLink(element) {
+    var id = element.id
+    if (id === "github") {
+        window.open("https://github.com/super-okay", '_blank');
+    }
+    else if (id === "linkedin") {
+        window.open("https://www.linkedin.com/in/allen-n-wang/", '_blank');
+    }
+    else if (id === "photography") {
+        window.open("https://www.flickr.com/photos/ramenoverdose/albums/72157696885465250", '_blank');
+    }
+}
