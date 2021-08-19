bb1252eced7731e5db4edd4ff7d23e4e2256b08e
super-okay <wangallen98@gmail.com>
2021-08-18 23:57:08 -0700

    mobile browser check

--- indexScript.js
+++ indexScript.js
@@ -1,3 +1,4 @@
+// opens link
 function openLink(element) {
     var id = element.id
     if (id === "github") {
@@ -11,5 +12,31 @@ function openLink(element) {
     }
     else if (id === "instagram") {
         window.open("https://www.instagram.com/superokay.img/", '_blank');
+    }
+}
+
+// checks for mobile browser using touch points
+function checkMobile() {
+    var hasTouchScreen = false;
+    if ("maxTouchPoints" in navigator) {
+        hasTouchScreen = navigator.maxTouchPoints > 0;
+    }
+    else if ("msMaxTouchPoints" in navigator) {
+        hasTouchScreen = navigator.msMaxTouchPoints > 0;
+    }
+
+    if (!hasTouchScreen) {
+        document.getElementById("main-div").style.boxShadow = "8px 8px 16px 0 rgba(0, 0, 0, 0.25), -4px -4px 12px 0 rgba(101, 101, 101, 0.3)";
+        document.getElementById("main-div").style.borderRadius = "15px";
+        document.getElementById("main-div").style.margin = "auto";
+        document.getElementById("main-div").style.marginTop = "10vh";
+        document.getElementById("main-div").style.padding = "20px";
+        document.getElementById("main-div").style.maxWidth = "1024px";
+        document.getElementById("main-div").style.minHeight = "500px";
+    }
+    else {
+        document.getElementById("main-div").style.margin = "auto";
+        document.getElementById("main-div").style.marginTop = "10vh";
+        document.getElementById("main-div").style.padding = "20px";
     }
 }
